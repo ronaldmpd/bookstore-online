a10c8b55cb955f5d242cbb5d4a5d0364f047095b
Ronald Pinto <ronald.pinto.d@gmail.com>
2021-03-17 15:55:21 -0400

    add sequelize db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,14 @@
 const PORT = process.env.PORT || 3000;
+const development =
+  {
+    "username": process.env.USERNAME || "root",
+    "password": process.env.PASSWORD || "",
+    "database": process.env.DATABASE || "bookstore-online-dev",
+    "host": process.env.HOST || "127.0.0.1",
+    "dialect": process.env.DIALECT || "mysql"
+  };
 
 module.exports = {
-    PORT
+    PORT,
+    development
 };
