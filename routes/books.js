1cd45261e10ef868f9a53b881c1769b18c3aa62e
Ronald Pinto <ronald.pinto.d@gmail.com>
2021-03-25 21:18:07 -0400

    add swagger for books api

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,69 @@
 const express = require('express');
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Book: 
+ *        type: object
+ *        required: 
+ *          - title
+ *          - description
+ *          - price
+ *          - state
+ *          - authorId
+ *        properties:
+ *          id:
+ *            type: number
+ *            description: id autogenerado
+ *          title:
+ *            type: string
+ *            description: title of book
+ *          description:
+ *            type: string
+ *            description: description of book 
+ *          price:
+ *            type: number
+ *            description: price of book 
+ *          state:
+ *            type: boolean
+ *            description: state of book (activo / inactivo)
+ *        example:
+ *          title: Java
+ *          description: Programming Java
+ *          price: 40.00
+ *          state: true
+ *          authorId: 1
+ */
 const app = express();
 const { getBooks, getBookById, addBook, updateBook, deleteBook } = require('../services/BookService');
 
+/**
+ * @swagger
+ * /books:
+ *  get:
+ *    tags:
+ *      - Books
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - in: query
+ *        name: from
+ *        type: number
+ *      - in: query
+ *        name: limit
+ *        type: number 
+ *    responses:
+ *      '200':
+ *        description: list of books paginations
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Book'
+ *      '400':
+ *        description: Error
+ */
 app.get("/books", async (req, res) => {
     try {
       let from = req.query.from || 0;
@@ -18,7 +80,30 @@ app.get("/books", async (req, res) => {
     }    
   });
 
-
+/**
+ * @swagger
+ * /books/{bookId}:
+ *  get:
+ *    tags:
+ *      - Books
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - in: path
+ *        name: bookId
+ *        type: number
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: get book by Id
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/Book'
+ *      '404':
+ *        description: Error
+ */
 // GET (obtener un book por su id)
 app.get("/books/:bookId", async (req, res) => {
     try {
@@ -31,13 +116,35 @@ app.get("/books/:bookId", async (req, res) => {
     }
   });
 
+/**
+ * @swagger
+ * /books:
+ *  post:
+ *    tags:
+ *      - Books
+ *    produces:
+ *      - application/json
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Book'
+ *    responses:
+ *      '201':
+ *        description: author creado
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Book'
+ */
 //POST
 app.post('/books', async (req, res) => {
     console.log(req.body);
     try{
         const body = req.body;
-        const book = await addBook(body);
-        return res.json(book);
+        const book = await addBook(body);        
+        return res.status(201).json(book);
     }catch(e){
         console.log(e);
         res.status(400).json({
@@ -46,6 +153,35 @@ app.post('/books', async (req, res) => {
     }    
 })
 
+/**
+ * @swagger
+ *
+ * /books/{bookId}:
+ *      put:
+ *          tags:
+ *              - Books
+ *          produces:
+ *              - application/json
+ *          parameters:
+ *              - in: path
+ *                name: bookId
+ *                type: number 
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Book'
+ *          responses:
+ *              '200':
+ *                  description: book created
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Book'
+ *              '400':
+ *                  description: Error
+ */
 // PUT // UPDATE (actualizar un book)
 app.put("/books/:bookId", async (req, res) => {
   console.log(req.body);
@@ -60,7 +196,29 @@ app.put("/books/:bookId", async (req, res) => {
     }
   });
 
-
+/**
+ * @swagger
+ *
+ * /books/{bookId}:
+ *      delete:
+ *          tags:
+ *              - Books
+ *          produces:
+ *              - application/json
+ *          parameters:
+ *              - in: path
+ *                name: bookId
+ *                type: number 
+ *          responses:
+ *              '200':
+ *                  description: book delete
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Book'
+ *              '400':
+ *                  description: Error
+ */
 // DELETE (eliminar un book)
 app.delete("/books/:bookId", async (req, res) => {
   try {
